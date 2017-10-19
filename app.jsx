842fd695f9fae139ab5b0ae6217c6daf7389b3b0
milagrosMndoza <milagrosmndoza@gmail.com>
2017-10-19 17:29:49 -0500

    subiendo mi repositorio

---
+++ app.jsx
@@ -0,0 +1,90 @@
+class Model {
+    constructor() {
+        this.comments = [];
+        this.inputValue = null;
+        this.callback = null;
+    }
+    subscribe(render) {
+        this.callback = render;
+    }
+
+    notify() {
+        this.callback();
+    }
+    addComments() {
+    console.log(this.inputValue.value);
+    if(this.inputValue!= null && this.inputValue.value !=''){
+        this.comments.push({
+            name: this.inputValue.value,
+            id: Utils.uuid(),
+        });
+        this.inputValue.value = '';
+        this.notify();
+    }
+    }
+    deleteComments(id) {
+        for (let i in this.comments)
+            if (this.comments[i].id == id) {
+                this.comments.splice(i , 1);
+            }
+        this.notify();
+    }
+
+    clickCheckbox(e, index){
+      this.comments[index].clase= (e.target.checked) ? "responded": "";
+      this.notify();
+    }
+}
+const CommentsList = ({
+        title,
+        model
+    }) => {return ( <ul> {
+                model.comments.map((commentId, index) => {
+                    return <li  key = {commentId.id} className={commentId.clase}> 
+                            {commentId.name}<br/>
+                    <button onClick = { () => model.deleteComments(commentId.id)
+                        } > Remove Comment </button>
+                           </li> ;
+                })
+            } </ul>);
+        }
+
+const CommentsApp = ({title,model}) => {
+            return ( <div className = "wrapper" >
+                <header >
+                 <p> Add Comment </p> <form onSubmit = {
+                    (e) => {
+                        e.preventDefault();
+                        model.addComments()
+                    }
+                } >
+                <input type = "text"
+                placeholder = "comment"
+                onChange = {
+                    e => (model.inputValue = e.target)
+                }/>
+                <button type = "submit" > Submit </button> </form> </header>
+                 <div className = "main" >
+                < h2 > My coments </h2> <
+                CommentsList model = {model}
+                /> </div>
+            </div>
+            );
+        }
+
+        let COMENTARIOS = [];
+        let model = new Model(COMENTARIOS);
+        let counter = 1;
+
+        let render = () => {
+            ReactDOM.render( <
+                CommentsApp title = "RegistrationApp"
+                model = {model}
+                />,
+                document.getElementById("container")
+            );
+        };
+
+        model.subscribe(render);
+
+        render();
